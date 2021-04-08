821bea46827f457171499261486965e0e4b88e75
Philip Laine <philip.laine@xenit.se>
2021-04-08 17:22:13 +0200

    Remove components extra default list
    Add image controllers to e2e test
    
    Signed-off-by: Philip Laine <philip.laine@xenit.se>

--- examples/install/variables.tf
+++ examples/install/variables.tf
@@ -3,3 +3,9 @@ variable "target_path" {
   default     = "staging-cluster"
   description = "flux install target path"
 }
+
+variable "components_extra" {
+  type        = list(string)
+  default     =  []
+  description = "extra flux components"
+}
