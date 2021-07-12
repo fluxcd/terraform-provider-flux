584e7049d5ed7129bcb80bbe5e82d6538c3e5e48
Philip Laine <philip.laine@gmail.com>
2021-07-12 22:47:37 +0200

    Fix file path
    Signed-off-by: Philip Laine <philip.laine@gmail.com>

--- examples/kustomize-flux/main.tf
+++ examples/kustomize-flux/main.tf
@@ -12,6 +12,6 @@ terraform {
 resource "github_repository_file" "kustomize" {
   repository = github_repository.main.name
   file       = data.flux_sync.main.kustomize_path
-  content    = file("${path.module}/templates/kustomization-override.yaml")
+  content    = file("${path.module}/kustomization-override.yaml")
   branch     = var.branch
 }
