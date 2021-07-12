7a9d41c59b37230756e8c01aeae4fc3b2108b1f0
Philip Laine <philip.laine@gmail.com>
2021-07-12 22:46:24 +0200

    Fix lint errors
    Signed-off-by: Philip Laine <philip.laine@gmail.com>

--- examples/kustomize-flux/main.tf
+++ examples/kustomize-flux/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    github = {
+      source = "integrations/github"
+      version = "4.5.2"
+    }
+  }
+}
+
 resource "github_repository_file" "kustomize" {
   repository = github_repository.main.name
   file       = data.flux_sync.main.kustomize_path
