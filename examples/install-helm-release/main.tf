e9c987692317a722d42f32e6ab2a5c2c4e41731b
Philip Laine <philip.laine@gmail.com>
2023-04-02 19:08:05 +0200

    Add guide for installing Flux using Terraform and the Helm chart
    Signed-off-by: Philip Laine <philip.laine@gmail.com>

---
+++ examples/install-helm-release/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">=2.9.0"
+    }
+    kind = {
+      source  = "tehcyx/kind"
+      version = ">=0.0.16"
+    }
+  }
+}
+
+provider "kind" {}
+
+resource "kind_cluster" "this" {
+  name = "flux-e2e"
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = kind_cluster.this.endpoint
+    client_certificate     = kind_cluster.this.client_certificate
+    client_key             = kind_cluster.this.client_key
+    cluster_ca_certificate = kind_cluster.this.cluster_ca_certificate
+  }
+}
+
+resource "helm_release" "this" {
+  repository       = "https://fluxcd-community.github.io/helm-charts"
+  chart            = "flux2"
+  version          = "2.7.0"
+  name             = "flux2"
+  namespace        = "flux-system"
+  create_namespace = true
+}
