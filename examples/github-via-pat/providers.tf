40bcf05ace635879aa4633f69838faca1a831f23
Steven Wade <steven@stevenwade.co.uk>
2024-04-08 21:48:33 +0100

    Fix: GitHub via PAT e2e test.
    Signed-off-by: Steven Wade <steven@stevenwade.co.uk>

--- examples/github-via-pat/providers.tf
+++ examples/github-via-pat/providers.tf
@@ -8,8 +8,8 @@ provider "flux" {
   git = {
     url = "https://github.com/${var.github_org}/${var.github_repository}.git"
     http = {
-      username    = "git" # This can be any string when using a personal access token
-      private_key = var.github_token
+      username = "git" # This can be any string when using a personal access token
+      password = var.github_token
     }
   }
 }
