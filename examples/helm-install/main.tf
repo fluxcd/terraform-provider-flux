a0469758ed2246b0e909303d488b6a8efe7d25df
Steven Wade <steven@stevenwade.co.uk>
2024-04-08 16:31:14 +0100

    Updating helm-chart example to include flux2-sync chart.
    Signed-off-by: Steven Wade <steven@stevenwade.co.uk>

--- examples/helm-install/main.tf
+++ examples/helm-install/main.tf
@@ -18,6 +18,10 @@ terraform {
       source  = "tehcyx/kind"
       version = ">= 0.4"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.27"
+    }
     tls = {
       source  = "hashicorp/tls"
       version = ">= 4.0"
@@ -25,17 +29,33 @@ terraform {
   }
 }
 
-# ==========================================
+# =============================================================================================
 # Construct KinD cluster
-# ==========================================
+# =============================================================================================
 
 resource "kind_cluster" "this" {
   name = "flux-e2e"
 }
 
 # ==========================================
-# Add deploy key to GitHub repository
+# Initialise a Github project
 # ==========================================
+
+resource "github_repository" "this" {
+  name        = var.github_repository
+  description = var.github_repository
+  visibility  = "public"
+  auto_init   = true
+}
+
+resource "github_branch_default" "this" {
+  repository = github_repository.this.name
+  branch     = "main"
+}
+
+# =============================================================================================
+# Add deploy key to GitHub repository
+# =============================================================================================
 
 resource "tls_private_key" "flux" {
   algorithm   = "ECDSA"
@@ -44,29 +64,84 @@ resource "tls_private_key" "flux" {
 
 resource "github_repository_deploy_key" "this" {
   title      = "Flux"
-  repository = var.github_repository
+  repository = github_repository.this.name
   key        = tls_private_key.flux.public_key_openssh
   read_only  = "false"
 }
 
-# ===============================================
-# Bootstrap KinD cluster using flux helm chart
-# ===============================================
+# =============================================================================================
+# Bootstrap KinD cluster using flux2 helm chart. This chart simply installs flux2
+# Ref: https://artifacthub.io/packages/helm/fluxcd-community/flux2
+# =============================================================================================
 
-# Ref: https://github.com/fluxcd-community/helm-charts/tree/main/charts/flux2
-resource "helm_release" "this" {
+resource "kubernetes_namespace" "flux_system" {
+  metadata {
+    name = "flux-system"
+  }
+}
+
+resource "helm_release" "flux2" {
   repository = "https://fluxcd-community.github.io/helm-charts"
   chart      = "flux2"
   version    = "2.12.4"
 
-  name             = "flux2"
-  namespace        = "flux-system"
-  create_namespace = true
+  name      = "flux2"
+  namespace = "flux-system"
 
-  # Allows us to upgrade the version of flux from another repo and terraform not roll it back.
-  lifecycle {
-    ignore_changes = [
-      version
-    ]
+  depends_on = [kubernetes_namespace.flux_system]
+}
+
+# =============================================================================================
+# Bootstrap KinD cluster using flux2-sync helm chart to start reconciliation of resources.
+# Ref https://artifacthub.io/packages/helm/fluxcd-community/flux2-sync
+# flux2-sync is used to setup the GitRepository and Kustomization resources
+# =============================================================================================
+
+resource "kubernetes_secret" "ssh_keypair" {
+  metadata {
+    name      = "ssh-keypair"
+    namespace = "flux-system"
   }
+
+  type = "Opaque"
+
+  data = {
+    "identity.pub" = tls_private_key.flux.public_key_openssh
+    "identity"     = tls_private_key.flux.private_key_pem
+    "known_hosts"  = "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg="
+  }
+
+  depends_on = [kubernetes_namespace.flux_system]
+}
+
+resource "helm_release" "flux2_sync" {
+  repository = "https://fluxcd-community.github.io/helm-charts"
+  chart      = "flux2-sync"
+  version    = "1.8.2"
+
+  # Note: Do not change the name or namespace of this resource. The below mimics the behaviour of "flux bootstrap".
+  name      = "flux-system"
+  namespace = "flux-system"
+
+  set {
+    name  = "gitRepository.spec.url"
+    value = "ssh://git@github.com/${var.github_org}/${var.github_repository}.git"
+  }
+
+  set {
+    name  = "gitRepository.spec.ref.branch"
+    value = github_branch_default.this.branch
+  }
+
+  set {
+    name  = "gitRepository.spec.secretRef.name"
+    value = kubernetes_secret.ssh_keypair.metadata[0].name
+  }
+
+  set {
+    name  = "gitRepository.spec.interval"
+    value = "1m"
+  }
+
+  depends_on = [helm_release.flux2]
 }
